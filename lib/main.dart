dcd72293d9bcc5d40953ab9f38a5e610f396dc81
clmokaya <mokmakori@gmail.com>
2025-04-16 11:48:11 +0300

    starting ui and theme management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/pages/homepage.dart';
+import 'package:habit_tracker/theme/dark_mode.dart';
+import 'package:habit_tracker/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(//theme provider
+      ChangeNotifierProvider(
+          create: (context) => Themeprovider(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +16,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Homepage();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Homepage(),
+      theme: Provider.of<Themeprovider>(context).themeData,
+    );
   }
 }
---
+++ lib/theme/dark_mode.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+        background: Colors.grey.shade900,
+        primary: Colors.grey.shade700,
+        secondary: Colors.white,
+        inversePrimary: Colors.grey.shade200));
